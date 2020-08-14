f15642a4f8d82fad1e51267d49120032e8dbd4c7
ashokbrolia <ashokbrolia@outlook.com>
2020-08-14 14:38:39 -0400

    1 file modified

--- src/app/fetch-data/fetch-data.component.ts
+++ src/app/fetch-data/fetch-data.component.ts
@@ -20,19 +20,19 @@ export class FetchDataComponent {
          {
         "id": 2,
         "name": "Ashok 2",
-        "phone": "111111111",
+        "phone": "222222222",
         "email": "test@gmail.com"
          },
          {
         "id": 3,
         "name": "Ashok 3",
-        "phone": "111111111",
+        "phone": "333333333",
         "email": "test@gmail.com"
          },
          {
         "id": 4,
         "name": "Ashok 4",
-        "phone": "111111111",
+        "phone": "444444444",
         "email": "test@gmail.com"
          },
        ]
