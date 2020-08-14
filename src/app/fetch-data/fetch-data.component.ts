f48cf6491957c32a801aa9efce5dfccd27b97559
ashokbrolia <ashokbrolia@outlook.com>
2020-08-14 14:36:47 -0400

    1 file modified

--- src/app/fetch-data/fetch-data.component.ts
+++ src/app/fetch-data/fetch-data.component.ts
@@ -7,7 +7,37 @@ import { ClaimForm } from '../claim-form/claimform';
   templateUrl: './fetch-data.component.html'
 })
 export class FetchDataComponent {
-  public claimforms: ClaimForm[];
-  
+  // public claimforms: ClaimForm[];
+  public claimforms: any;
+   constructor(){
+       this.claimforms = [
+         {
+        "id": 1,
+        "name": "Ashok 1",
+        "phone": "111111111",
+        "email": "test@gmail.com"
+         },
+         {
+        "id": 2,
+        "name": "Ashok 2",
+        "phone": "111111111",
+        "email": "test@gmail.com"
+         },
+         {
+        "id": 3,
+        "name": "Ashok 3",
+        "phone": "111111111",
+        "email": "test@gmail.com"
+         },
+         {
+        "id": 4,
+        "name": "Ashok 4",
+        "phone": "111111111",
+        "email": "test@gmail.com"
+         },
+       ]
+       
+   }
+
 }
 
